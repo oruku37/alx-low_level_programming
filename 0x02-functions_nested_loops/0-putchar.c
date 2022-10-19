49f4acfc8825e56dcf4c089d45a735b360cd18f1
Alabi-Williams Isaac <oruku37a@gmail.com>
2022-10-19 12:30:51 -0700

    latest fixes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,13 @@
 #include "main.h"
 
 /**
-
  * main - prints _putchar, fllowd by a new linees
-
  * Return: Alwys 0 (Success)
-
  */
 
 int main(void)
 {
 	char abe[] = "_putchar";
-
 	int c;
 
 	for (c = 0; c < 8; c++)
