f7f6a6fa888b6b042723bf21b13904b471a67b4e
Alabi-Williams Isaac <oruku37a@gmail.com>
2022-10-20 16:01:40 -0700

    check for capital letter

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,14 @@
 /*
 * main - entry
 *
+* _isupper - checks if parameter(c) is a capital letter
 * Return: 0
 */
 
-int _isuppper(int c)
+int _isupper(int c)
 {
-	if(c >= 65 && c <= 90)
-	return(1);
+	if (c >= 65 && c <= 90)
+	return (1);
 	else
-	return(0);
-}	
+	return (0);
+}
