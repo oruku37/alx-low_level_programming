9da70cf04ca4f47bc4ad759ced3a9425cd310c46
Alabi-Williams Isaac <oruku37a@gmail.com>
2022-10-25 13:55:02 -0700

    first commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - function to change a variable from its pointer
- * @n - pointer 
+ * @n: pointer
  *
  * Return: null
  */
@@ -10,5 +10,4 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
-	
 }
