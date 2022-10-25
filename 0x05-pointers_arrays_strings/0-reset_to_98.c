25514997b379458450d60e88f2ac74cb1da367f1
Alabi-Williams Isaac <oruku37a@gmail.com>
2022-10-25 13:52:03 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to change a variable from its pointer
+ * @n - pointer 
+ *
+ * Return: null
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n)
+
+#endif
