569d97efec0a76720be00dfff15e434cc9aee682
Alabi-Williams Isaac <oruku37a@gmail.com>
2022-10-25 14:09:39 -0700

    completed task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values in two variables
+ * @a: variable to be swapped
+ * @b: variable to be swapped
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = 0;
+
+	c = *a
+	*a = *b
+	*b = c
+}
