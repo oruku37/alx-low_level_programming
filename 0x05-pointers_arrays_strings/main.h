b219e99c60492aa7b9512b8f9e595987159a21fa
Alabi-Williams Isaac <oruku37a@gmail.com>
2022-10-25 15:41:25 -0700

    fixed task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,13 @@
 #ifdef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void _puts(char *str)
+void _puts(char *str);
 
 #endif
